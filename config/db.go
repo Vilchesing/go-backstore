0a18a32e858888bbc5d06e83a6dd3b759f51cf8f
Brayan Vilches <brayanvilchese@gmail.com>
2025-08-01 15:02:42 -0600

    Se modifico db.go,  Se define un numero maximo de conexiones que pueden estar abiertas a la vez, se agrego un numero maximo de conexiones inactivas en espera. por ultimo si la conexion a la base de datos es incorrecta cierra la conexion

--- config/db.go
+++ config/db.go
@@ -8,18 +8,15 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var DB *sql.DB
-
-func InitDB() {
-	var err error
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true",
+func InitDB() (sql.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_NAME"),
 	)
 
-	DB, err = sql.Open("mysql", dsn)
+	db, err = sql.Open("mysql", dsn)
 	if err != nil {
 		panic(err)
 	}
